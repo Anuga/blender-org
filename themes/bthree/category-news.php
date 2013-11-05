7bfabdba968ded2865911815481265ee5a4b1d17
Pablo Vazquez <venomgfx@gmail.com>
2013-11-05 15:37:17 +0100

    News tweaks

--- themes/bthree/category-news.php
+++ themes/bthree/category-news.php
@@ -48,24 +48,60 @@
 		<div class="span8">
 			<div class="row-fluid">
 				<div class="span6 box">
-					<h2>Blender Network</h2>
+					<h2>Blender Network<small class="clearfix">Professionals Network</small></h2>
 					<?php echo do_shortcode('[wp_rss_aggregator source="377" limit="5"]'); ?>
 				</div>
 				<div class="span6 box">
-					<h2>BlenderNation</h2>
-					<?php echo do_shortcode('[wp_rss_aggregator source="1589" limit="5"]'); ?>
+					<h2>Developers Blog<small class="clearfix">code.blender.org</small></h2>
+					<?php echo do_shortcode('[wp_rss_aggregator source="1828" limit="5"]'); ?>
 				</div>
 			</div>
 
 			<div class="row-fluid">
 				<div class="span6 box">
-					<h2>Developers Blog</h2>
-					<?php echo do_shortcode('[wp_rss_aggregator source="1828" limit="5"]'); ?>
+					<h2>BlenderNation<small class="clearfix">Community News</small></h2>
+					<?php echo do_shortcode('[wp_rss_aggregator source="1589" limit="5"]'); ?>
 				<hr/>
 				</div>
 				<div class="span6 box">
-					<h2>Some other items</h2>
-					<?php echo do_shortcode('[wp_rss_aggregator]'); ?>
+					<h2>Other Community News<small class="clearfix">Stay up to date with everything Blender</small></h2>
+					<ul class="rss-aggregator">
+						<li class="feed-item">
+							<a target="_blank" href="https://plus.google.com/+AdventuresInBlender/">
+								Adventures in Blender</a>
+							<div class="source-date">
+								<span class="feed-source">News & Reviews</span>
+							</div>
+						</li>
+						<li class="feed-item">
+							<a target="_blank" href="http://blender.stackexchange.com/">
+								Blender Stack Exchange</a>
+							<div class="source-date">
+								<span class="feed-source">Community powered Blender Q&A</span>
+							</div>
+						</li>
+						<li class="feed-item">
+							<a target="_blank" href="http://www.blenderartists.org/">
+								BlenderArtists</a>
+							<div class="source-date">
+								<span class="feed-source">Where most Blender artists and coders gather</span>
+							</div>
+						</li>
+						<li class="feed-item">
+							<a target="_blank" href="http://www.blendswap.com/">
+								BlendSwap</a>
+							<div class="source-date">
+								<span class="feed-source">Blend Files Online Repository</span>
+							</div>
+						</li>
+						<li class="feed-item">
+							<a target="_blank" href="http://www.graphicall.org/">
+								GraphicAll.org</a>
+							<div class="source-date">
+								<span class="feed-source">Unofficial Blender builds made by the community</span>
+							</div>
+						</li>
+					</ul>
 				<hr/>
 				</div>
 			</div>
